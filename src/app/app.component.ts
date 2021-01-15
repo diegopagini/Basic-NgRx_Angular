63c8d0f29c2af4edfbd0d90b065e55c6cbf6f427
Diego Pagini <dq@qbkconsulting.com>
2021-01-15 17:59:00 -0300

    ahora con NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'redux-app';
 
   contador: number;
 
-  constructor() {
-    this.contador = 10;
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.store.subscribe((state) => {
+      console.log(state);
+      this.contador = state.contador;
+    });
   }
 
   incrementar() {
-    this.contador++;
+    this.store.dispatch(actions.incrementar());
   }
 
   decrementar() {
-    this.contador--;
+    this.store.dispatch(actions.decrementar());
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+//NgRx
+import { StoreModule } from '@ngrx/store';
+import { contadorReducer } from './contador/contador.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HijoComponent,
-    NietoComponent
-  ],
+  declarations: [AppComponent, HijoComponent, NietoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({ contador: contadorReducer }),
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
